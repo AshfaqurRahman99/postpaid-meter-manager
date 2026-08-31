9e0295ea025b384ca0d78678dd64deb9db05394b
AshfaurRahman99 <ashfaqur99rahman@gmail.com>
2026-08-31 11:02:39 +0600

    Added googleapis dependency

--- next.config.ts
+++ next.config.ts
@@ -2,12 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   typescript: {
-    // Vercel-কে টাইপ এরর ইগনোর করতে নির্দেশ দিচ্ছে
     ignoreBuildErrors: true,
-  },
-  eslint: {
-    // Vercel-কে ESLint এরর ইগনোর করতে নির্দেশ দিচ্ছে
-    ignoreDuringBuilds: true,
   },
 };
 
